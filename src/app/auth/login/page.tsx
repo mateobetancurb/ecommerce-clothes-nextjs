46e94b2dc562970f5c7f04f69eff2395f5c35198
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-14 18:51:09 -0500

    feat: Create login page component

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1 className="text-6xl">login page</h1>
+		</div>
+	);
+}
