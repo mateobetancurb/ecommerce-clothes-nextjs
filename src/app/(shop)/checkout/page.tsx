8426328fa00551880c1afd3df5364f97e2ed371e
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-14 19:12:32 -0500

    feat: Add CheckoutPage component for the shop checkout page

---
+++ src/app/(shop)/checkout/page.tsx
@@ -0,0 +1,7 @@
+export default function CheckoutPage() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1 className="text-6xl">checkout page</h1>
+		</div>
+	);
+}
