0e2105805b022ea894d0f2ec449409b6f2868f42
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-14 19:12:23 -0500

    feat: Add CartPage component for the shop cart page

---
+++ src/app/(shop)/cart/page.tsx
@@ -0,0 +1,7 @@
+export default function CartPage() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1 className="text-6xl">cart page</h1>
+		</div>
+	);
+}
