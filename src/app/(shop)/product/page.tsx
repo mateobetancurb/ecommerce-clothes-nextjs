fb49a3194ad4fc32b3695fa2f14e0dd5f82dad94
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-14 19:17:56 -0500

    feat: Remove ProductPage component from the shop product page

--- src/app/(shop)/product/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProductPage() {
-	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<h1 className="text-6xl">product page</h1>
-		</div>
-	);
-}
