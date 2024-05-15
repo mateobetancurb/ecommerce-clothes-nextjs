95fe5f73081e7de4ed2d1087a3a1da24f4d14b9a
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-14 19:12:19 -0500

    feat: Add admin page component for the shop admin area

---
+++ src/app/(shop)/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1 className="text-6xl">admin page</h1>
+		</div>
+	);
+}
