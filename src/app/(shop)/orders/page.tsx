bb6e01cca1a58f6bf700aed5c03368c89353dffe
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-14 19:12:46 -0500

    feat: Add OrdersPage component for the shop orders page

---
+++ src/app/(shop)/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function OrdersPage() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1 className="text-6xl">orders page</h1>
+		</div>
+	);
+}
