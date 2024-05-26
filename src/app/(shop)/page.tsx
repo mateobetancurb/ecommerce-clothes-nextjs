288b7cd1acd07f85c3eeaef2c7fbb07cf601b70f
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-26 13:05:02 -0500

    feat: Add title component to shop page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,7 +1,9 @@
+import { Title } from "@/components";
+
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<h1 className="text-6xl">Welcome to Next.js!</h1>
-		</div>
+		<>
+			<Title title="Tienda" subtitle="Todos los productos" />
+		</>
 	);
 }
