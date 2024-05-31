0d9aa6467ced894d2e57e8d842444667fd1cd8c6
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-31 18:29:56 -0500

    refactor: Adjust width of Sidebar component in ShopLayout

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ export const Sidebar = () => {
 			<div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30"></div>
 			<div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"></div>
 
-			<nav className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
+			<nav className="fixed p-5 right-0 top-0 w-[350px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
@@ -22,7 +22,6 @@ export const Sidebar = () => {
 						d="M6 18 18 6M6 6l12 12"
 					/>
 				</svg>
-
 				<NavigationLink
 					linkInfo={{
 						url: "/",
@@ -46,7 +45,6 @@ export const Sidebar = () => {
 						),
 					}}
 				/>
-
 				<NavigationLink
 					linkInfo={{
 						url: "/",
@@ -69,7 +67,6 @@ export const Sidebar = () => {
 						),
 					}}
 				/>
-
 				<NavigationLink
 					linkInfo={{
 						url: "/",
@@ -92,7 +89,6 @@ export const Sidebar = () => {
 						),
 					}}
 				/>
-
 				<NavigationLink
 					linkInfo={{
 						url: "/",
@@ -116,7 +112,74 @@ export const Sidebar = () => {
 					}}
 				/>
 
-				<div className="w-full h-px bg-gray-200 my-10" />
+				<div className="w-full h-px bg-gray-200 my-5" />
+
+				<NavigationLink
+					linkInfo={{
+						url: "/",
+						text: "Productos",
+						icon: (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="size-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+								/>
+							</svg>
+						),
+					}}
+				/>
+				<NavigationLink
+					linkInfo={{
+						url: "/",
+						text: "Órdenes",
+						icon: (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="size-6 "
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
+								/>
+							</svg>
+						),
+					}}
+				/>
+				<NavigationLink
+					linkInfo={{
+						url: "/",
+						text: "Usuarios",
+						icon: (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="size-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+								/>
+							</svg>
+						),
+					}}
+				/>
 			</nav>
 		</div>
 	);
