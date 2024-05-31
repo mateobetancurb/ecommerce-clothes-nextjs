aea0c74e01a49e2fbe896cb15a897bb4adf90923
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-30 19:33:29 -0500

    feat: Add ProductGrid component to shop page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,13 @@
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
 	return (
 		<>
 			<Title title="Tienda" subtitle="Todos los productos" />
+			<ProductGrid products={products} />
 		</>
 	);
 }
