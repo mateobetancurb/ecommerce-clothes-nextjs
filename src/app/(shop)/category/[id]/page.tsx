06ed7d436126a8de8484c04b74db2f30da7e60f1
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-02 12:38:41 -0500

    feat: Update CategoryPage to display products based on category ID

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { notFound } from "next/navigation";
+import { initialData } from "@/seed/seed";
+import { ProductGrid, Title } from "@/components";
 
 interface Props {
 	params: {
@@ -7,13 +9,17 @@ interface Props {
 }
 
 export default function CategoryPage({ params }: Props) {
+	const productsByCategory = initialData.products.filter(
+		(product) => product.gender === params.id
+	);
+
 	if (params.id === "kids") {
 		notFound();
 	}
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<h1 className="text-6xl">category page {params.id}</h1>
+			<ProductGrid products={productsByCategory} />
 		</div>
 	);
 }
