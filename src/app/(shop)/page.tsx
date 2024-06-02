2c527f319f150cbd53b7750dba81a5aa78d1fb70
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-02 13:48:00 -0500

    refactor: Update Home page layout and add responsive styling

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -5,9 +5,8 @@ const products = initialData.products;
 
 export default function Home() {
 	return (
-		<main className="sm:mt-20">
+		<main className="mt-20 w-[90%] mx-auto">
 			<Title title="Inicio" subtitle="Todos los productos" />
-
 			<ProductGrid products={products} />
 		</main>
 	);
