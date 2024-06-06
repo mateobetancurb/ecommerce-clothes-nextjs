bbe8c98adb40935433816fe9c1a95860a8d6d48e
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-06 12:29:02 -0500

    feat: Add QuantitySelector component to ProductPage

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
 
 export * from "./product/size-selector/SizeSelector";
+export * from "./product/quantity-selector/QuantitySelector";
 
 export * from "./ui/sidebar/Sidebar";
 export * from "./global/NavigationLink";
