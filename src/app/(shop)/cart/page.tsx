c785ef014a579f4dd4f80e86a570ce94af790ecd
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-07 18:01:34 -0500

    refactor: Add lazy loading for images in CartPage

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,87 @@
+import Link from "next/link";
+import Image from "next/image";
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const productsInCart = [
+	initialData.products[0],
+	initialData.products[1],
+	initialData.products[2],
+];
+
 export default function CartPage() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<h1 className="text-6xl">cart page</h1>
+		<div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+			<div className="flex flex-col w-[1000px]">
+				<Title title="Resumen de tu compra" className="mt-24 text-xl" />
+				<div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+					{/* cart */}
+					<div className="flex space-y-5 flex-col mt-5">
+						<Link
+							href={"/"}
+							className="mb-5 w-fit hover:bg-black hover:text-white transition-all flex items-center p-2 rounded-md gap-3"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="size-5"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+								/>
+							</svg>
+							Volver
+						</Link>
+
+						{/* items */}
+						{productsInCart.map((product) => (
+							<div key={product.slug} className="flex">
+								<Image
+									src={`/products/${product.images[1]}`}
+									alt={product.title}
+									width={150}
+									height={150}
+									className="mr-5 rounded"
+								/>
+								<div>
+									<div className="flex gap-20">
+										<p>{product.title}</p>
+										<p className="font-bold text-xl">${product.price}</p>
+									</div>
+									<div className="flex items-center justify-between">
+										<QuantitySelector quantity={3} />
+
+										<button className="hover:bg-red-300 hover:p-2 transition-all rounded-full">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												strokeWidth={1.5}
+												stroke="currentColor"
+												className="size-6"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+												/>
+											</svg>
+										</button>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+
+					{/* checkout */}
+					<div className=""></div>
+				</div>
+			</div>
 		</div>
 	);
 }
