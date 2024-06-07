0c5b1f35b6e07852eb08f77bedb08bed0076256d
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-07 16:53:51 -0500

    refactor: Update TopMenu component to use SVG icon for menu button

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -77,7 +77,20 @@ export const TopMenu = () => {
 					onClick={openMenu}
 					className="m-2 p-2 rounded-md transition-all hover:bg-gray-100 hover:text-black"
 				>
-					Menú
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="size-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+						/>
+					</svg>
 				</button>
 			</div>
 		</nav>
