9a0dece035f3c3f7aecd3530d1b702f0c09b9548
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-08 13:39:31 -0500

    refactor: Add Sidebar and Footer components to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export * from "./ui/top-menu/TopMenu";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
+export * from "./ui/sidebar/Sidebar";
+export * from "./ui/footer/Footer";
 
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
@@ -10,5 +12,4 @@ export * from "./product/quantity-selector/QuantitySelector";
 export * from "./product/slide/SlideDesktop";
 export * from "./product/slide/SlideMobile";
 
-export * from "./ui/sidebar/Sidebar";
 export * from "./global/NavigationLink";
