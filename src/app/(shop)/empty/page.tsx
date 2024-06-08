d505dbe0c28f3881a2f40315b01405844ad3d481
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-08 12:48:17 -0500

    refactor: Update EmptyPage layout, add SVG illustration and link to store

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function EmptyPage() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<h1 className="text-6xl">empty page</h1>
+		<div className="flex justify-center items-center h-[800px]">
+			<div className="flex flex-col items-center">
+				<div className="flex gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="size-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+						/>
+					</svg>
+					<h1 className="text-xl font-semibold mb-10">Tu carrito está vacío</h1>
+				</div>
+
+				<Link href={"/"} className="btn-primary">
+					Ir a la tienda
+				</Link>
+			</div>
 		</div>
 	);
 }
