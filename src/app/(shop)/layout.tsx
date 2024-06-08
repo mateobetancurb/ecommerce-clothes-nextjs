d3384dcf8569b50babf658f2cb15b230e5f2fc52
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-08 13:39:26 -0500

    refactor: Update ShopLayout to include Footer component

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar, TopMenu } from "@/components";
+import { Footer, Sidebar, TopMenu } from "@/components";
 
 export default function ShopLayout({
 	children,
@@ -6,10 +6,13 @@ export default function ShopLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<main className="min-h-screen">
-			<TopMenu />
-			<Sidebar />
-			<div className="px-0 sm:px-10">{children}</div>
-		</main>
+		<div className="min-h-screen">
+			<main>
+				<TopMenu />
+				<Sidebar />
+				<div className="px-0 sm:px-10">{children}</div>
+			</main>
+			<Footer />
+		</div>
 	);
 }
