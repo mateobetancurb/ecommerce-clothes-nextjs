0191900715870bee793179a03302326754599d23
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-08 10:44:00 -0500

    refactor: Update AddressPage layout and add input fields for shipping information

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,7 +1,66 @@
+import Link from "next/link";
+import { Title } from "@/components";
+
 export default function AddressPage() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<h1 className="text-6xl">address page</h1>
+		<div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
+			<div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
+				<Title title="Datos de envío" className="md:mt-28 text-2xl" />
+
+				<div className="grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2">
+					<div className="flex flex-col mb-2">
+						<span>Nombres</span>
+						<input type="text" className="p-2 border rounded-md bg-gray-200" />
+					</div>
+
+					<div className="flex flex-col mb-2">
+						<span>Apellidos</span>
+						<input type="text" className="p-2 border rounded-md bg-gray-200" />
+					</div>
+
+					<div className="flex flex-col mb-2">
+						<span>Dirección</span>
+						<input type="text" className="p-2 border rounded-md bg-gray-200" />
+					</div>
+
+					<div className="flex flex-col mb-2">
+						<span>Dirección 2 (opcional)</span>
+						<input type="text" className="p-2 border rounded-md bg-gray-200" />
+					</div>
+
+					<div className="flex flex-col mb-2">
+						<span>Código postal</span>
+						<input type="text" className="p-2 border rounded-md bg-gray-200" />
+					</div>
+
+					<div className="flex flex-col mb-2">
+						<span>Ciudad</span>
+						<input type="text" className="p-2 border rounded-md bg-gray-200" />
+					</div>
+
+					<div className="flex flex-col mb-2">
+						<span>País</span>
+						<select className="p-2 border rounded-md bg-gray-200">
+							<option value="">[ Seleccionar ]</option>
+							<option value="CRI">Costa Rica</option>
+						</select>
+					</div>
+
+					<div className="flex flex-col mb-2">
+						<span>Teléfono</span>
+						<input type="text" className="p-2 border rounded-md bg-gray-200" />
+					</div>
+
+					<div className="flex flex-col mb-2 sm:mt-10">
+						<Link
+							href="/checkout"
+							className="btn-primary flex w-full sm:w-1/2 justify-center "
+						>
+							Siguiente
+						</Link>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
