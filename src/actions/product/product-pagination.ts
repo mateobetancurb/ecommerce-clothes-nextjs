9290bf63e77a387285c60b1ff057ca1d0dd3bdba
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-14 13:14:31 -0500

    feat: Update pagination options for fetching paginated products with images

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -2,9 +2,22 @@
 
 import prisma from "@/lib/prisma";
 
-export const getPaginatedProductsWithImages = async () => {
+interface PaginationOptions {
+	page?: number;
+	take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+	page = 1,
+	take = 12,
+}: PaginationOptions) => {
+	if (isNaN(Number(page))) page = 1;
+	if (page < 1) page = 1;
+
 	try {
 		const products = await prisma.product.findMany({
+			take: take,
+			skip: (page - 1) * take,
 			include: {
 				ProductImage: {
 					take: 2,
@@ -15,9 +28,11 @@ export const getPaginatedProductsWithImages = async () => {
 			},
 		});
 
-		console.log(products);
+		const totalProducts = await prisma.product.count();
 
 		return {
+			currentPage: page,
+			totalPages: Math.ceil(totalProducts / take),
 			products: products.map((product) => ({
 				...product,
 				images: product.ProductImage.map((image) => image.url),
