903be874158360c3bc09d12ddd38bde4c39e5a9b
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-14 13:44:49 -0500

    feat: Add lazy loading for images in Pagination component

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,10 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 
 interface Props {
 	totalPages: number;
 }
 
 export const Pagination = ({ totalPages }: Props) => {
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+	const currentPage = Number(searchParams.get("page") ?? 1);
+
+	const createPageUrl = (pageNumber: number | string) => {
+		const params = new URLSearchParams();
+		if (pageNumber === "...") {
+			return `${pathname}?${params.toString()}`;
+		}
+
+		if (+pageNumber === 0) {
+			return `${pathname}`;
+		}
+
+		if (+pageNumber > totalPages) {
+			return `${pathname}?${params.toString()}`;
+		}
+
+		params.set("page", pageNumber.toString());
+		return `${pathname}?${params.toString()}`;
+	};
+
 	return (
 		<div className="flex justify-center text-center mb-32 mt-10">
 			<nav aria-label="Page navigation example">
@@ -12,7 +37,7 @@ export const Pagination = ({ totalPages }: Props) => {
 					<li className="page-item disabled">
 						<Link
 							className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-							href="#"
+							href={createPageUrl(currentPage - 1)}
 							aria-disabled="true"
 						>
 							<svg
@@ -58,7 +83,7 @@ export const Pagination = ({ totalPages }: Props) => {
 					<li className="page-item">
 						<Link
 							className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-							href="#"
+							href={createPageUrl(currentPage + 1)}
 						>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
