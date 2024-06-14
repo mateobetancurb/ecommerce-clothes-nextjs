6e2fe9b51a9b3de45b8f1ee48021e25bb5b1299a
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-14 13:14:37 -0500

    feat: Update Home page to fetch paginated products with images

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,14 +1,27 @@
-import { ProductGrid, Title } from "@/components";
+import { Pagination, ProductGrid, Title } from "@/components";
 import { getPaginatedProductsWithImages } from "@/actions";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-	const { products } = await getPaginatedProductsWithImages();
-	console.log(products);
+interface Props {
+	searchParams: {
+		page?: string;
+	};
+}
+
+export default async function Home({ searchParams }: Props) {
+	const page = searchParams.page ? Number(searchParams.page) : 1;
+	const { products, currentPage, totalPages } =
+		await getPaginatedProductsWithImages({ page });
+
+	if (products.length === 0) {
+		redirect("/");
+	}
 
 	return (
 		<main className="mt-20 w-[90%] mx-auto">
 			<Title title="Inicio" subtitle="Todos los productos" />
 			<ProductGrid products={products} />
+			<Pagination totalPages={totalPages} />
 		</main>
 	);
 }
