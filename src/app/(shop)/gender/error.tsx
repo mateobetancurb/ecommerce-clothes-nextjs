af6f936c19eff5374ad0760b5f9530fe3c282d2f
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-17 14:23:29 -0500

    feat: Add gender error page for category not found

---
+++ src/app/(shop)/gender/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { PageNotFound } from "@/components";
+
+export default function NotFound() {
+	return <PageNotFound text="Lo sentimos, esta categoría no existe" url="/" />;
+}
+``;
