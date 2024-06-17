e9419bfca9434ae2a439c74ca057534eb8191744
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-17 14:19:50 -0500

    refactor: Update gender category links in TopMenu component

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -16,19 +16,19 @@ export const TopMenu = () => {
 
 			<div className="hidden sm:block">
 				<Link
-					href={"/category/men"}
+					href={"/gender/men"}
 					className="m-2 p-2 rounded-md transition-all hover:bg-gray-100 hover:text-black"
 				>
 					Hombres
 				</Link>
 				<Link
-					href={"/category/women"}
+					href={"/gender/women"}
 					className="m-2 p-2 rounded-md transition-all hover:bg-gray-100 hover:text-black"
 				>
 					Mujeres
 				</Link>
 				<Link
-					href={"/category/kid"}
+					href={"/gender/kid"}
 					className="m-2 p-2 rounded-md transition-all hover:bg-gray-100 hover:text-black"
 				>
 					Niños
