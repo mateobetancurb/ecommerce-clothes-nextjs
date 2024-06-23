4b8b80dbe4289fe7c91c38172f5ba071207c0eff
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-23 17:16:19 -0500

    feat: Add currencyFormat utility function for formatting currency values

---
+++ src/utils/currencyFormat.ts
@@ -0,0 +1,8 @@
+export const currencyFormat = (value: number) => {
+	return new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+		maximumFractionDigits: 2,
+		minimumFractionDigits: 2,
+	}).format(value);
+};
