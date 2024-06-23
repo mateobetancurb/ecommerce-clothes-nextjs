38479f49088d0beaa941edef4e22b1ad216ee4bc
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-22 23:14:52 -0500

    refactor: Update SizeSelector component to handle selected size and size change event

--- src/components/product/size-selector/SizeSelector.tsx
+++ src/components/product/size-selector/SizeSelector.tsx
@@ -1,11 +1,16 @@
 import type { Size } from "@/interfaces";
 
 interface Props {
-	selectedSize: Size;
+	selectedSize?: Size;
 	availableSizes: Size[];
+	onSizeChanged: (size: Size) => void;
 }
 
-export const SizeSelector = ({ selectedSize, availableSizes }: Props) => {
+export const SizeSelector = ({
+	selectedSize,
+	availableSizes,
+	onSizeChanged,
+}: Props) => {
 	return (
 		<div className="mb-10">
 			<h3 className="font-bold mb-2">Tallas disponibles</h3>
@@ -13,9 +18,10 @@ export const SizeSelector = ({ selectedSize, availableSizes }: Props) => {
 				{availableSizes.map((size) => (
 					<button
 						key={size}
+						onClick={() => onSizeChanged(size)}
 						className={`mx-2 hover:underline text-lg ${
 							size === selectedSize
-								? "bg-gray-200 font-bold p-1 rounded-sm"
+								? "bg-gray-200 transition-all font-bold p-1 rounded-sm"
 								: ""
 						}`}
 					>
