30ada2d8d13edb36bc385dc5dd2938cb10c55b82
Mateo Betancur B <mateobetancurb@gmail.com>
2024-06-22 23:14:49 -0500

    refactor: Update QuantitySelector component to fix quantity change buttons

--- src/components/product/quantity-selector/QuantitySelector.tsx
+++ src/components/product/quantity-selector/QuantitySelector.tsx
@@ -22,26 +22,6 @@ export const QuantitySelector = ({ quantity }: Props) => {
 			<h2 className="font-bold mb-2">Cantidad</h2>
 			<div className="flex items-center gap-4 ">
 				<button
-					onClick={() => onQuantityChange(+1)}
-					className="hover:bg-green-300 rounded-full transition-all"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth={1.5}
-						stroke="currentColor"
-						className="size-8"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-						/>
-					</svg>
-				</button>
-				<span className="text-xl">{counter}</span>
-				<button
 					onClick={() => onQuantityChange(-1)}
 					className="hover:bg-red-300 rounded-full transition-all"
 				>
@@ -57,6 +37,26 @@ export const QuantitySelector = ({ quantity }: Props) => {
 							strokeLinecap="round"
 							strokeLinejoin="round"
 							d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+						/>
+					</svg>
+				</button>
+				<span className="text-xl">{counter}</span>
+				<button
+					onClick={() => onQuantityChange(+1)}
+					className="hover:bg-green-300 rounded-full transition-all"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="size-8"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
 						/>
 					</svg>
 				</button>
