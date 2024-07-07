d0baffd1e4b35a031055e3001b0ddd78bd320719
Mateo Betancur B <mateobetancurb@gmail.com>
2024-07-07 09:19:37 -0500

    refactor: Update OrderSummary component to use useRouter hook for navigation

--- src/app/(shop)/cart/ui/OrderSummay.tsx
+++ src/app/(shop)/cart/ui/OrderSummay.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Link from "next/link";
 import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useCartStore } from "@/store";
 import { currencyFormat } from "@/utils";
-import { useRouter } from "next/navigation";
 
 export const OrderSummay = () => {
 	const router = useRouter();
