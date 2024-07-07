ffc7a5a52486ac8765a93f6f360e927c2f30c714
Mateo Betancur B <mateobetancurb@gmail.com>
2024-07-07 11:49:57 -0500

    refactor: Seed database with initial users

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -3,12 +3,17 @@ import prisma from "../lib/prisma";
 
 async function main() {
 	// clean up the database before seeding
+	await prisma.user.deleteMany();
 	await prisma.productImage.deleteMany();
 	await prisma.product.deleteMany();
 	await prisma.category.deleteMany();
 
 	// categories created in DB
-	const { categories, products } = initialData;
+	const { categories, products, users } = initialData;
+
+	await prisma.user.createMany({
+		data: users,
+	});
 
 	const categoriesData = categories.map((name) => ({ name }));
 
