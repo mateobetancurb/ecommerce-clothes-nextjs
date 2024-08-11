9d311c340040ae2a37241cc99712f3fecfa85f82
Mateo Betancur B <mateobetancurb@gmail.com>
2024-08-11 09:36:44 -0500

    refactor: Update page component to use dynamic routing for product ID

--- src/app/(shop)/product/[id]/page.tsx
+++ src/app/(shop)/product/[id]/page.tsx
@@ -1,10 +1,9 @@
-export const revalidate = 604800;
-
 import { notFound } from "next/navigation";
 import type { Metadata, ResolvingMetadata } from "next";
+import Image from "next/image";
 import { QuantitySelector, Slide, SlideMobile } from "@/components";
 import { AddToCart } from "./ui/AddToCart";
-// import { getProductById } from "@/actions";
+import { getProductById } from "@/actions/products/get-product-by-id";
 
 interface Props {
 	params: {
@@ -12,67 +11,67 @@ interface Props {
 	};
 }
 
-// export async function generateMetadata(
-// 	{ params }: Props,
-// 	parent: ResolvingMetadata
-// ): Promise<Metadata> {
-// 	const slug = params.slug;
-// 	const product = await getProductBySlug(slug);
+export async function generateMetadata(
+	{ params }: Props,
+	parent: ResolvingMetadata
+): Promise<Metadata> {
+	const id = params.id;
+	const product = await getProductById({ id });
 
-// 	return {
-// 		title: product?.title ?? "Producto no encontrado",
-// 		description: product?.description ?? "",
-// 		openGraph: {
-// 			title: product?.title ?? "Producto no encontrado",
-// 			description: product?.description ?? "",
-// 			images: [`/products/${product?.images[1]}`],
-// 		},
-// 	};
-// }
+	return {
+		title: product?.data.title ?? "Producto no encontrado",
+		description: product?.data.description ?? "",
+		openGraph: {
+			title: product?.data.title ?? "Producto no encontrado",
+			description: product?.data.description ?? "",
+			images: [`/products/${product?.data.image}`],
+		},
+	};
+}
 
 export default async function ProductPage({ params }: Props) {
-	console.log(params.id);
+	const { id } = params;
 
-	// const { id } = params;
-	// const product = await getProductById(id);
-	// console.log(product);
+	const product = await getProductById({ id });
 
-	// if (!product) {
-	// 	notFound();
-	// }
+	if (!product) {
+		notFound();
+	}
 
 	return (
-		<div>
-			<h1 className="mt-32">ProductPage {params.id}</h1>
+		<div className="mt-32 md:mb-32 grid grid-cols-1 md:grid-cols-2 gap-3">
+			{/* <div className="col-span-1 md:col-span-2 ">
+				<SlideMobile
+					title={product.data.title}
+					images={product.data.image}
+					className="block md:hidden"
+				/>
+
+				<Slide
+					title={product.data.title}
+					images={product.data.image}
+					className="hidden md:block"
+				/>
+			</div> */}
+			<Image
+				src={product.data.image}
+				height={400}
+				width={400}
+				alt={product.data.title}
+				className="mx-auto"
+			/>
+
+			<div className="mt-10 mb-20 col-span-1 px-5 ">
+				<h1 className="antialiased font-bold text-xl">{product.data.title}</h1>
+				<p className="text-lg mb-5 ">${product.data.price}</p>
+
+				{/* <StockLabel slug={product.slug} /> */}
+
+				{/* <AddToCart product={product} /> */}
+
+				<h3 className="font-bold">Descripción</h3>
+				<p>{product.data.description}</p>
+			</div>
 		</div>
-		// <div className="sm:my-20 grid md:grid-cols-3 gap-3">
-		// 	<div className="col-span-1 md:col-span-2 ">
-		// 		{/* mobile slide */}
-		// 		<SlideMobile
-		// 			title={product.title}
-		// 			images={product.images}
-		// 			className="block md:hidden"
-		// 		/>
-
-		// 		{/* desktop slide */}
-		// 		<Slide
-		// 			title={product.title}
-		// 			images={product.images}
-		// 			className="hidden md:block"
-		// 		/>
-		// 	</div>
-
-		// 	<div className="col-span-1 px-5 ">
-		// 		<h1 className="antialiased font-bold text-xl">{product.title}</h1>
-		// 		<p className="text-lg mb-5 ">${product.price}</p>
-
-		// 		<StockLabel slug={product.slug} />
-
-		// 		<AddToCart product={product} />
-
-		// 		<h3 className="font-bold">Descripción</h3>
-		// 		<p>{product.description}</p>
-		// 	</div>
-		// </div>
 	);
 }
