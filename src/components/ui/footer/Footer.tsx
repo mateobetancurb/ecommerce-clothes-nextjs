8416842d527c02d7916c2489a72d9b5aeb8320b4
Mateo Betancur B <mateobetancurb@gmail.com>
2024-08-11 09:36:52 -0500

    refactor: Update footer component to use semantic HTML and improve accessibility

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,13 +1,19 @@
 export const Footer = () => {
 	return (
-		<div className="bg-black h-10 text-white flex items-center gap-1 w-full justify-center text-xs">
-			<span>
-				Desarrollado por{" "}
-				<a href="https://www.linkedin.com/in/mateobetancurb/" target="_bank">
-					Mateo |
-				</a>
-			</span>
-			<p>Derechos reservados © {new Date().getFullYear()}</p>
-		</div>
+		<footer className="bg-black text-white py-2 w-full mt-auto">
+			<div className="container mx-auto flex items-center justify-center text-xs">
+				<span>
+					Desarrollado por{" "}
+					<a
+						href="https://www.linkedin.com/in/mateobetancurb/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Mateo |
+					</a>
+				</span>
+				<p className="ml-1">Derechos reservados © {new Date().getFullYear()}</p>
+			</div>
+		</footer>
 	);
 };
